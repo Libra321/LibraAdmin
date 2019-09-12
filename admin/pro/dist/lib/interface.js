f32878d2929e2c5dbd079ff856e62d942e86b140
阿东 <783331585@qq.com>
2019-09-12 18:03:38 +0800

    Add files via upload

---
+++ admin/pro/dist/lib/interface.js
@@ -0,0 +1,8 @@
+;layui.define(function(exports){
+    var api={
+            "user-getUserInfo":"https://localhost:44362/user/getUserInfo",
+            "user-getUserMenu":"https://localhost:44362/user/getUserMenu",
+            "new-getNew":""
+    };
+    exports("api",api);
+});
